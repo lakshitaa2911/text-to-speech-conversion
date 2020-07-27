ea1310a8be2d958e56d3c4f5cd64d39be609dfdd
lakshitaa2911 <55542701+lakshitaa2911@users.noreply.github.com>
2020-07-27 21:46:35 +0530

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,28 @@
+# to speech conversion 
+from gtts import gTTS 
+  
+# This module is imported so that we can  
+# play the converted audio 
+import os 
+  
+# The text that you want to convert to audio 
+#mytext = "This is awesome as always"
+fh = open("test.txt", "r")
+mytext = fh.read().replace("\n", " ")
+  
+# Language in which you want to convert 
+language = 'en'
+  
+# Passing the text and language to the engine,  
+# here we have marked slow=False. Which tells  
+# the module that the converted audio should  
+# have a high speed 
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+  
+# Saving the converted audio in a mp3 file named 
+# welcome  
+myobj.save("new.mp3") 
+fh.close()
+  
+# Playing the converted file 
+os.system("start new.mp3")
